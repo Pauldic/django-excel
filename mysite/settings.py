c337c9fbf23ed6aab3d5b8977d15290b388c6b69
Paul Okeke <pauldiconline@gmail.com>
2021-12-06 14:12:21 +0100

    File System

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,8 +9,9 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-import os
+import os, sys
 import django
+from core.decouple import config, Csv
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
@@ -19,28 +20,52 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&kzg)(b_xt1*nyp-s97ibt01b7p+67#&-brmb_nqj&dn_%q6tc'
+# SECRET_KEY = '&kzg)(b_xt1*nyp-s97ibt01b7p+67#&-brmb_nqj&dn_%q6tc'
+SECRET_KEY = config('SECRET_KEY')
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+DEBUG = config('DEBUG', default=True, cast=bool)
+LOGGING_LEVEL = config('LOGGING_LEVEL', default='INFO')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='*', cast=Csv())
 
+PROJECT_NAME = 'mysite'
+TEMPLATE_THEME = 'default'
+
+SITE_ID = 1
+ADMINS = (('Administrator', 'support@gmbenefit.org'), ('Tester', 'donationmove@gmail.com'),)
+MANAGERS = ADMINS
+
+
+# APPEND_SLASH = True
+# ADMIN_URL = 'access/'
+# LOGIN_URL = 'account:login'
+# LOGOUT_URL = 'account:logout'
+# LOGIN_REDIRECT_URL = 'core:dashboard'
+# LOGOUT_REDIRECT_URL = 'core:index'
 # Application definition
 
 FILE_UPLOAD_HANDLERS = ("django_excel.ExcelMemoryFileUploadHandler",
                         "django_excel.TemporaryExcelFileUploadHandler")
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.humanize',
+    'django.contrib.sites',
     'django.contrib.staticfiles',
+    
     'core.apps.CoreConfig',
     'import_export'
-)
+]
+
+if DEBUG:
+    INSTALLED_APPS.append("sslserver")
+
 
 COMMON_MIDDLEWARE = (
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -121,9 +146,125 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, PROJECT_NAME + "/" + TEMPLATE_THEME)]
 
+DEEP_STATIC_COLLECT = config('DEEP_STATIC_COLLECT', default=True, cast=bool)
+if DEEP_STATIC_COLLECT:
+    STATICFILES_DIRS.append(os.path.join(BASE_DIR, PROJECT_NAME + "/assets"))
+    
+    
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'filters': {
+        'require_debug_false': {
+            '()': 'django.utils.log.RequireDebugFalse'
+        }
+    },
+
+    'formatters': {
+        'verbose': {
+            # ERROR|25/Apr/2017 17:30:22|basehttp|25069|139803081283328|"POST /system/secured/admin/core/package/add/ HTTP/1.1" 500 59
+            'format': '%(levelname)s|%(asctime)s|%(module)s|%(process)d|%(thread)d|%(message)s',
+            'datefmt': "%Y-%d-%b %H:%M:%S"
+        },
+        'standard': {
+            # [ERROR]25-Apr-2017 17:52:24|django.server|"POST /system/secured/admin/core/package/add/ HTTP/1.1" 500 59
+            'format': '[%(levelname)s]%(asctime)s|%(name)s|%(message)s',
+            'datefmt': "%Y-%m-%d %H:%M:%S"
+        },
+        'simple': {'format': '%(levelname)s|%(asctime)s|%(message)s', 'datefmt': "%Y-%m-%d %H:%M:%S"},
+        'too-simple': {'format': '%(message)s'},
+    },
+
+    'handlers': {
+        'null': {
+            'level': LOGGING_LEVEL,
+            'class': 'logging.NullHandler',
+        },
+        'mail_admins': {
+            'level': LOGGING_LEVEL,
+            'filters': ['require_debug_false'],
+            'class': 'django.utils.log.AdminEmailHandler',
+            'include_html': True,
+            'email_backend': 'django.core.mail.backends.smtp.EmailBackend',
+        },
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'too-simple',
+            'stream': sys.stdout,
+        },
+        'log_file': {
+            'level': LOGGING_LEVEL,
+            'class': 'logging.handlers.RotatingFileHandler',
+            'filename': os.path.join(BASE_DIR, PROJECT_NAME + '.log'),
+            'maxBytes': 1024 * 1024 * 5,  # 5MB
+            'backupCount': 5,
+            'formatter': 'standard',
+        },
+    },
+
+    'loggers': {
+        PROJECT_NAME + '.tasks': {
+            'handlers': ['mail_admins'],
+            'propagate': True,
+        },
+        # PROJECT_NAME: {
+        #     'handlers': ['mail_admins', 'console', 'log_file'],
+        #     'propagate': True,
+        # },
+        # 'core.*.*': {
+        #     'handlers': ['console'],
+        #     'propagate': True,
+        # },
+        # 'core.overrides': {
+        #     'handlers': ['console'],
+        #     'propagate': True,
+        # },
+        # 'core.*': {
+        #     'handlers': ['mail_admins', 'console', 'log_file'],
+        #     'propagate': True,
+        # },
+        PROJECT_NAME+'.*': {
+            'handlers': ['mail_admins', 'console', 'log_file'],
+            'propagate': True,
+        },
+        'django.db.backends': {
+            'handlers': ['log_file'],
+            'propagate': True,
+        },
+        'django.security.*': {
+            'handlers': ['mail_admins', 'log_file'],
+            'propagate': True,
+        },
+        'django.security.DisallowedHost': {
+            'handlers': ['null'],
+            'propagate': False,
+        },
+        'django_redis.cache': {
+            'handlers': ['log_file', 'console'],
+            'propagate': True,
+        },
+        'django.request': {
+            'handlers': ['mail_admins', 'log_file'],
+            'propagate': True,
+        },
+        'django': {
+            'handlers': ['log_file', 'console'],
+            'propagate': True,
+        },
+        'root': {
+            'handlers': ['console'],
+        }
+    },
+}
+
+# USE_THOUSAND_SEPARATOR = True
+# THOUSAND_SEPARATOR = ','
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from core.views import site_work_summary
+from core.views import list_invoices
 
 
 urlpatterns = [
+    url(r'^$', list_invoices, ),
     url(r'^admin/', admin.site.urls),
     url(r'^core/', include('core.urls'))
 ]
